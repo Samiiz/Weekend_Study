d65c22e24117468311f48e021327217dcf9d2865
Seou0912 <myuni0912@gmail.com>
2024-01-27 16:15:11 +0900

    0127 수박 시저암호

--- Week2/Kakao Secret Map.py
+++ Week2/Kakao Secret Map.py
@@ -15,3 +15,10 @@ def solution(n, arr1, arr2):
         answer.append(b)
     return answer
 
+def solution(n, arr1, arr2):
+    answer = []
+    for a, b in zip(arr1, arr2):
+        z=bin(a | b)[2:].zfill(n)
+        z=z.replace("0"," "),replace("1","#")
+    answer.append(z)
+    return answer        
